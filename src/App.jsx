e4f7701f08681c5127bb81df1a14ae2ec6fd41b9
mark mwendwa <mwendwamark310@gmail.com>
2024-01-05 11:54:23 +0300

    file set up, apply global css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p> Welcome </p>
+        <span>hello</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
